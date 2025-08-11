304930011f4cebfcae4e34af4773329741a9fa6f
shinaakram20 <shinaakram20@gmail.com>
2025-08-11 20:43:11 +0500

    :fire: init commit

--- src/server.js
+++ src/server.js
@@ -12,11 +12,10 @@ import commentRoutes from './routes/comments.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['https://trello-task-fe.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
