6afd78c1ffd35a78ed4951be054043a7327a481f
shinaakram20 <shinaakram20@gmail.com>
2025-08-11 23:09:48 +0500

    :fire: fixed cors

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://kanban-app-frontend.vercel.app'],
+  origin: ['https://trello-task-fe.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
